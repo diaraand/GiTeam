14769071bbb737db99d219cb0481fd5410cf18e4
Anna Riso <annariso@MacBook-Air-di-Anna-4.local>
2024-05-10 23:31:34 +0200

    PEP8

--- backend/app/mymodules/Public_Private.py
+++ backend/app/mymodules/Public_Private.py
@@ -7,7 +7,7 @@ def Public_Private(file_path):
 # Open the csv file
     with open(file_path, newline='', encoding='ISO-8859-1') as file_csv:
         lettore = csv.DictReader(file_csv, delimiter=';')
-#Iteratation and get university, enrolled students and gender
+# Iteratation and get university, enrolled students and gender
         for riga in lettore:
             ateneo_nome = riga['AteneoNOME']
             numero_iscritti = int(riga['Isc'])
@@ -15,7 +15,7 @@ def Public_Private(file_path):
 # if the university it's not present: add it to the dictionary
             if ateneo_nome not in risultato:
                 risultato[ateneo_nome] = {'M': 0, 'F': 0}
-#update the count of enrolled students for the corresponding gender              
+# update the count of enrolled students for the corresponding gender
             risultato[ateneo_nome][sesso] += numero_iscritti
-#return the final result   
+# return the final result
     return risultato
