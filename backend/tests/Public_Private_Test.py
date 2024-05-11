98c7be548f3b6ce644e65c5fd8287e0ca06d5e47
Anna Riso <annariso@MacBook-Air-di-Anna-4.local>
2024-05-11 11:26:31 +0200

    PEP8 has been checked

--- backend/tests/Public_Private_Test.py
+++ backend/tests/Public_Private_Test.py
@@ -1,16 +1,18 @@
 import sys
 import unittest
-# adding the directory 
+# adding the directory
 sys.path.append('/app')
-file_path = '/app/app/ds752_iscritti_anno_accademico_2017-18.csv' # file path to the csv file
+file_path = '/app/app/ds752_iscritti_anno_accademico_2017-18.csv'   # file path to the csv file
 # import the function to test
 from app.mymodules.Public_Private import Public_Private
 
+
 # define a test class
 class TestPublicPrivate(unittest.TestCase):
-# define a test method
+
+    # define a test method
     def test(self):
-# call the function to test
+        # call the function to test
         result = Public_Private(file_path)
         self.assertEqual(result, {
                                 "Milano Politecnico": {"M": 29448, "F": 14590},
@@ -20,7 +22,8 @@ class TestPublicPrivate(unittest.TestCase):
                                 "Milano Bicocca": {"M": 12892, "F": 2017},
                                 "Milano San Raffaele": {"M": 1024, "F": 1578},
                                 "Milano IULM": {"M": 1672, "F": 4070},
-                                "Rozzano (MI) Humanitas University": {"M": 285,"F": 439}})
+                                "Rozzano (MI) Humanitas University": {"M": 285, "F": 439}})
+
 
 # entry point for running the test
 if __name__ == '__main__':
